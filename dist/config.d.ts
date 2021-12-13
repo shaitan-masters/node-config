0fb551771501bb8732f9605e25c01d62374ea117
Alexander Gropyanov <fasderq@gmail.com>
2021-12-13 10:27:49 +0300

    update +fix

--- dist/config.d.ts
+++ dist/config.d.ts
@@ -1,7 +1,7 @@
 import { SchemaType } from './types';
 export declare class Config {
     private readonly config;
-    constructor(configPath: string, schema: SchemaType);
+    constructor(configPath: string | undefined, schema: SchemaType);
     get(): any;
     private defineConfig;
     private processConfig;
