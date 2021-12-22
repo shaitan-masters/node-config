8b49c5d7ce083e0b5fe4b79e6d5bb1c094af4abe
VasiliyIsaichkin <vasiliy.isaichkin@mojam.co>
2021-12-22 03:02:10 +0300

    Fix complie rules

--- dist/config.d.ts
+++
@@ -1,8 +0,0 @@
-import { SchemaType } from './types';
-export declare class Config {
-    private readonly config;
-    constructor(configPath: string | undefined, schema: SchemaType);
-    get(): any;
-    private defineConfig;
-    private processConfig;
-}
--- dist/types.d.ts
+++
@@ -1,1 +0,0 @@
-export declare type SchemaType = (cb: <T>(envName: string, defValue: T) => T) => object;
