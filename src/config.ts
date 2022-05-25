923bea84658d9d10480945ccb23d3ed4c2e88d7b
VasiliyIsaichkin <vasiliy@isaichkin.ru>
2022-05-25 14:54:46 +0300

    Change in constructor, replace configPath > configOverride

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,15 @@
 import oTools from '@osmium/tools';
-import {existsSync} from 'fs';
 
 export type SchemaDefine<K> = (cb: <T>(envName: string, defValue: T) => T) => K;
+export type DeepPartial<T> = T extends object ? {
+	[P in keyof T]?: DeepPartial<T[P]>;
+} : T;
 
-export class Config<SchemaType> {
+export class Config<SchemaType extends { [key: string | number]: any }> {
 	private readonly config: SchemaType;
 
-	constructor(configPath: string | undefined = undefined, schema: SchemaDefine<SchemaType> = () => ({} as SchemaType)) {
-		let configFileData = {};
-
-		if (configPath && existsSync(configPath)) {
-			configFileData = require(configPath);
-		}
-
-		this.config = this.processConfig(schema(this.defineConfig), configFileData);
+	constructor(schema: SchemaDefine<SchemaType> = () => ({} as SchemaType), configOverride?: DeepPartial<SchemaType>) {
+		this.config = this.processConfig(schema(this.defineConfig), configOverride || {} as DeepPartial<SchemaType>);
 	}
 
 	get(): SchemaType {
@@ -25,12 +21,16 @@ export class Config<SchemaType> {
 			switch (typeof defValue) {
 				case 'number':
 					return parseFloat(data);
+
 				case 'boolean':
 					return data.toLowerCase().indexOf('true') !== -1;
+
 				case 'object':
 					return JSON.parse(data);
+
 				case 'string':
 					return data;
+
 				default:
 					return data;
 			}
@@ -41,7 +41,7 @@ export class Config<SchemaType> {
 		return (envVal !== undefined ? _parseEnvValue(envVal) : defValue);
 	}
 
-	private processConfig(struct: any, obj: object) {
+	private processConfig(struct: SchemaType, obj: DeepPartial<SchemaType>) {
 		return oTools.iterate(struct, (val, idx) => {
 			if (oTools.isObject(val)) return this.processConfig(val, obj[idx] === undefined ? {} : obj[idx]);
 
